74a165fe2df96629afd471c172bf5a6901ce2fc4
jlbuild <juliabuildbot@gmail.com>
2020-04-24 07:03:53 +0000

    rr_jll build 5.3.1+3

--- src/wrappers/x86_64-linux-gnu-cxx03.jl
+++ src/wrappers/x86_64-linux-gnu-cxx03.jl
@@ -2,6 +2,7 @@
 export rr
 
 using capnproto_jll
+using CompilerSupportLibraries_jll
 ## Global variables
 PATH = ""
 LIBPATH = ""
@@ -49,8 +50,8 @@ function __init__()
     append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
     # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (capnproto_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (capnproto_jll.LIBPATH_list,))
+    foreach(p -> append!(PATH_list, p), (capnproto_jll.PATH_list, CompilerSupportLibraries_jll.PATH_list,))
+    foreach(p -> append!(LIBPATH_list, p), (capnproto_jll.LIBPATH_list, CompilerSupportLibraries_jll.LIBPATH_list,))
 
     global rr_path = normpath(joinpath(artifact_dir, rr_splitpath...))
 
