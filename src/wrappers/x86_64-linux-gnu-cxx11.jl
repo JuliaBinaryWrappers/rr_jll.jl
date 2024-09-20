180665771a854b081ad287af8f36a208146799f7
jlbuild <juliabuildbot@gmail.com>
2024-09-20 21:16:49 +0000

    rr_jll build 5.8.0+0

--- src/wrappers/x86_64-linux-gnu-cxx11.jl
+++ src/wrappers/x86_64-linux-gnu-cxx11.jl
@@ -2,11 +2,12 @@
 export rr
 
 using Zlib_jll
+using Zstd_jll
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("rr")
 JLLWrappers.@declare_executable_product(rr)
 function __init__()
-    JLLWrappers.@generate_init_header(Zlib_jll, CompilerSupportLibraries_jll)
+    JLLWrappers.@generate_init_header(Zlib_jll, Zstd_jll, CompilerSupportLibraries_jll)
     JLLWrappers.@init_executable_product(
         rr,
         "bin/rr",
